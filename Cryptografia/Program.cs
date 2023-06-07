0d867449e0e932e870873d61dbddb6495073b397
Willian-Louback <williandlouback@gmail.com>
2023-06-07 00:54:41 -0300

    first commit

---
+++ Cryptografia/Program.cs
@@ -0,0 +1,55 @@
+﻿
+namespace Cryptografia
+{
+    class Program
+    {
+        static void Main()
+        {
+            Menu();
+        }
+
+        static public void Menu()
+        {
+            string message;
+            string key;
+            int keyNumber;
+
+            Console.WriteLine("O que você deseja fazer? 1. Criptografar || 2. Descriptografar");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Digite o texto que você deseja criptografar: ");
+                    message = Console.ReadLine();
+
+                    Console.WriteLine("Digite a sua chave secreta: ");
+                    key = Console.ReadLine();
+
+                    Console.WriteLine("Digite o seu número secreto: ");
+                    keyNumber = int.Parse(Console.ReadLine());
+
+                    Script.Criptografar(message, key, keyNumber);
+                    break;
+                case 2:
+                    Console.WriteLine("Digite o texto que você deseja descriptografar: ");
+                    message = Console.ReadLine();
+
+                    Console.WriteLine("Digite a sua chave secreta: ");
+                    key = Console.ReadLine();
+
+                    Console.WriteLine("Digite o seu número secreto: ");
+                    keyNumber = int.Parse(Console.ReadLine());
+
+                    Script.Descriptografar(message, key, keyNumber);
+                    break;
+                default:
+                    Console.WriteLine("Digite um valor valido!");
+                    Console.ReadLine();
+                    Console.Clear();
+                    Menu();
+                    break;
+            }
+        }
+    }
+}
---
+++ Cryptografia/Script.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Cryptografia
+{
+    public class Script
+    {
+        public static void Criptografar(string text, string key, int keyNumber)
+        {
+            string result = Calculate(text, key, keyNumber, "+");
+
+            Continue(result);
+        }
+
+        public static void Descriptografar(string text, string key, int keyNumber)
+        {
+            string result = Calculate(text, key, keyNumber, "-");
+
+            Continue(result);
+        }
+
+        static string Calculate(string text, string key, int keyNumber, string operador)
+        {
+            string result = "";
+            text = text.ToUpper();
+            key = key.ToUpper();
+            int j = 0;
+            int m = 0;
+
+            for(int k = 0; k < keyNumber; k++)
+            {
+                for (int i = 0; i < text.Length; i++, j++)
+                {
+                    j = (j == key.Length) ? 0 : j;
+                    char cT = text[i];
+                    char cK = key[j];
+                    int ascT = Convert.ToInt32(cT);
+                    int ascK = Convert.ToInt32(cK);
+                    int add = ascK - 65;
+
+
+                    if (ascT != 32)
+                    {
+                        ascT = (operador == "+") ? ascT += add : ascT -= add;
+
+                        if (ascT < 65)
+                        {
+                            ascT = (91 - (65 - ascT));
+                        }
+                        else if (ascT > 90)
+                        {
+                            ascT = (64 + (ascT - 90));
+                        }
+                    }
+
+                    result += Convert.ToChar(ascT);
+                }
+
+                if (m == 24)
+                {
+                    key = Changekey(key);
+                    m = 0;
+                }
+
+                m++;
+                text = result;
+                result = "";
+                j = 0;
+            }
+            
+            return text;
+        }
+
+        static string Changekey(string key)
+        {
+            char firstLetter = key[0];
+            key = key.Substring(1);
+            key += firstLetter;
+            return key;
+        }
+
+        static void Continue(string result)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Este é o seu resultado: ");
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+
+            Console.WriteLine("Deseja voltar ao menu? 1. Sim || 2. Não");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.Clear();
+                Program.Menu();
+            }
+        }
+    }
+}
